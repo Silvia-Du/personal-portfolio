944dcd3ce7bcfee53c5f82a27a5367f2469c064d
Silvia-Du <silviadu@hotmail.it>
2022-08-01 11:27:11 +0200

    Inseriti elementi dinamici nel footer

--- src/components/partials/FooterComp.vue
+++ src/components/partials/FooterComp.vue
@@ -1,5 +1,13 @@
 <template>
-  <footer class="debug">Foter</footer>
+  <footer class="debug py-5 d-flex align-items-center justify-content-center">
+    <div class="border py-4 px-5 text-center">
+      <div class="circle animate__swing">
+        <i class="fa-solid fa-envelope"></i>
+      </div>
+      <p class="title">Let's work together.</p>
+      <p>&copy; 2022 Designed by Silvia Dugato.</p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -11,7 +19,63 @@ export default {
 <style lang="scss" scoped >
 
 footer{
-  height: 80px;
+  
+  .border{
+    border: 1px solid black;
+
+    &:hover .circle{
+      animation-name: swing-letter;
+    }
+
+    .title{
+      font-size: 3rem;
+      font-weight: bold;
+      position: relative;
+      &:hover{
+        &::after{
+          animation-name: change-lenght;
+        }
+      }
+
+
+      &::after{
+        content: ' ';
+        display: table;
+        position: absolute;
+        height: 5px;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.884);
+        animation-duration: 1.5s;
+      }
+    }
+
+  .circle{
+    padding: 15px;
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 50%;
+    animation-duration: 2s;
+    .fa-envelope{
+      font-size: 2rem;
+      
+    }
+  }
+  }
+
+  
+  @keyframes change-lenght {
+    0%   {width: 90%;}
+    50%  {width: 5%;}
+    100%  {width: 100%;}
+  }
+
+  @keyframes swing-letter {
+    0%   {transform: rotate(30deg)}
+    25%  {transform: rotate(-30deg)}
+    50%  {transform: rotate(15deg)}
+    75%  {transform: rotate(-15deg)}
+    100% {transform: rotate(0deg)}
+  }
 }
 
 </style>
